27315ee6df1f4a7d1c6f4bd9edd10e097b51a01d
Paraskhilosiya <paraskhilosiyapkofficial@gmail.com>
2023-04-20 20:25:30 +0530

    only one is pending

--- LRU.cpp
+++ LRU.cpp
@@ -40,25 +40,14 @@ int find_maxitime(int timespace[], int f)
     }
     return ans;
 }
-int main()
-{
-    int fault = 0;
-    int n;
-    cout << "Enter Number Of Input Process : ";
-    cin >> n;
-    int request[n];
-    for (int i = 0; i < n; i++)
-        cin >> request[i];
-    int f;
-    cout << "Enter Number Of Frames : ";
-    cin >> f;
-    int frames[f] = {-1};
-    int timespace[f] = {-1};
 
+void LRU(int frames[], int f, int request[], int n)
+{
     int i = 0;
     int hit = 0;
     int curr_s = 0;
     int page_fault = 0;
+    int timespace[f] = {-1};
 
     while (curr_s < f)
     {
@@ -72,7 +61,6 @@ int main()
             frames[curr_s] = request[i];
             timespace[curr_s] = 0;
             curr_s++;
-            fault++;
         }
 
         for (int i = 0; i < f; i++)
@@ -102,7 +90,6 @@ int main()
             int maxi = find_maxitime(timespace, f);
             frames[maxi] = request[i];
             timespace[maxi] = 0;
-            fault++;
         }
 
         for (int i = 0; i < f; i++)
@@ -121,4 +108,77 @@ int main()
 
     cout << "Hits = " << hit << endl
          << "Miss = " << n - hit;
+}
+
+int kiskokaru(int frames[], int request[], int f, int t, int n)
+{
+    int maxi = -9999;
+    int ans = -1;
+    for (int i = 0; i < f; i++)
+    {
+        int j;
+        for (j = t + 1; j < n; j++)
+        {
+            if (frames[i] == request[j])
+                break;
+        }
+        if (j > maxi)
+        {
+            maxi = j;
+            ans = i;
+        }
+    }
+    return ans;
+}
+
+void Optimal(int frames[], int f, int request[], int n)
+{
+    int i = 0;
+    int curr_s = 0;
+    int hit = 0;
+    while (curr_s < f)
+    {
+        if (find(frames, f, request[i]) > 0)
+            hit++;
+        else
+        {
+            frames[curr_s] = request[i];
+            curr_s++;
+        }
+        display(frames, f);
+        cout << endl;
+        i++;
+    }
+
+    while (i < n)
+    {
+        if (find(frames, f, request[i]) >= 0)
+            hit++;
+        else
+        {
+            int dis = kiskokaru(frames, request, f, i, n);
+            frames[dis] = request[i];
+        }
+        display(frames, f);
+        cout << endl;
+        i++;
+    }
+    cout << "Hits = " << hit << endl
+         << "Miss = " << n - hit;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter Number Of Input Process : ";
+    cin >> n;
+    int request[n];
+    for (int i = 0; i < n; i++)
+        cin >> request[i];
+    int f;
+    cout << "Enter Number Of Frames : ";
+    cin >> f;
+    int frames[f] = {-1};
+    // LRU(frames, f, request, n);
+    Optimal(frames, f, request, n);
 }
